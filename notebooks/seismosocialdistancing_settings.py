a61e007f034f8d9826a9397599b0f35741dd5c84
lmoresi <>
2020-06-13 10:51:15 +1000

    Changing plots

--- notebooks/seismosocialdistancing_settings.py
+++ notebooks/seismosocialdistancing_settings.py
@@ -8,7 +8,7 @@ from obspy import UTCDateTime
 # Set these values to control the notebook behaviour
 
 # Make sure you take at least a full week (>=7 days) before the first "ban"
-start = UTCDateTime("2020-01-15")
+start = UTCDateTime("2019-12-01")
 
 # Leaving UTCDateTime() empty means "now":
 # and this means 24 hours ago: UTCDateTime() - 24*3600
@@ -28,10 +28,22 @@ dataset = "Australia_AUNRC"
 time_zone = "Australia/Brisbane"
 sitedesc = "Rockhampton, Queensland"
 
-data_provider = "IRIS"
+data_provider = "http://auspass.edu.au:8080"
 logo = None # 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Logo_SED_2014.png/220px-Logo_SED_2014.png'
 bans = {
-        "2020-01-28 00:00":"End of School Summer Holiday"
+        "2020-01-28 00:00":"End of School Summer Holiday",
+        "2020-03-18 00:00":'No Large Gatherings',
+        "2020-03-25 12:00":'Restaurants/Bars/Schools closed',
         }
 
+reference = {"start": "2020-01-28 00:00",
+              "end":  "2020-03-18 00:00"}
+
+summer_hol = {"start":"2019-12-20 00:00",
+              "end":  "2020-01-28 00:00"}
+
+lockdown   = {"start":"2020-03-19 00:00",
+              "end":  "2020-06-01 00:00"}
+
+reopening    = {"start":"2020-06-01 00:00"}
 
